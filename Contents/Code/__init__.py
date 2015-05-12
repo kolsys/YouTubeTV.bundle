463f9800726b27dcd90275bd2429a0e64e1dbda7
KOL <kol@nightmail.ru>
2015-05-13 00:35:14 +0300

    - Fix best format selection - Add live stream support

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -159,11 +159,7 @@ def MySubscriptions(offset=None):
         res = JSON.ObjectFromString(HTTP.Request(
             'https://m.youtube.com/%s?%s' % (path, urlencode(params)),
             headers={
-                'User-Agent': (
-                    'Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) '
-                    'AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 '
-                    'Mobile/11B554a Safari/9537.54'
-                )
+                'User-Agent': Video.USER_AGENT
             }
         ).content[4:])['content']
     except:
